7942be5fa69796afe706a91190059c7337fe861e
huda0209 <huda030209@gmail.com>
2022-04-17 00:39:21 +0900

    [dal]不要なimportと変数を削除

--- src/main/java/com/github/majisyou/maintenace/event/Player_Login.java
+++ src/main/java/com/github/majisyou/maintenace/event/Player_Login.java
@@ -1,18 +1,14 @@
 package com.github.majisyou.maintenace.event;
 
-import com.github.majisyou.maintenace.Maintenace;
 import com.github.majisyou.maintenace.system.ConfigManager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 
-import java.util.logging.Logger;
 
 public class Player_Login implements Listener {
-    static String Maintenance_mode;
 
     @EventHandler
     public void Login_cancel (PlayerJoinEvent event){
